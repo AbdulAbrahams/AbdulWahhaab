e914cd924fa89304c0c4e9864512d49ce5b19b78
AbdulAbrahams <abdulabrahams2002@gmail.com>
2023-04-17 15:19:24 +0200

    spinner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,82 +1,88 @@
 <template>
-  <div class="body">
-    <Navbar />
-    <div class="container">
-      <section class="introSec">
-        <div class="row introduction">
-          <div class="col-md-6 intro">
-            <h1 class="introH1">Abdul Wahhaab Abrahams</h1>
-            <h3 class="my-1">Web Developer</h3>
-            <a href="#about" class="btn btn-lg btn-light my-4 px-5" style="border-radius: 0px;">Learn About Me</a>
+  <div v-if="skills">
+    <div class="body">
+      <Navbar />
+      <div class="container">
+        <section class="introSec">
+          <div class="row introduction">
+            <div class="col-md-6 intro">
+              <h1 class="introH1">Abdul Wahhaab Abrahams</h1>
+              <h3 class="my-1">Web Developer</h3>
+              <a href="#about" class="btn btn-lg btn-light my-4 px-5" style="border-radius: 0px;">Learn About Me</a>
+            </div>
+            <div class="col-md-6 ImgCol my-5">
+              <img class="meImg" src="https://i.postimg.cc/sX56rV4K/Screenshot-2023-04-17-143529.png" alt="">
+            </div>
           </div>
-          <div class="col-md-6 ImgCol my-5">
-            <img class="meImg" src="https://i.postimg.cc/sX56rV4K/Screenshot-2023-04-17-143529.png" alt="">
+        </section>
+        <div class="space" id="about"></div>
+        <section class="aboutSec">
+          <h1 class="AboutH1">About Me</h1>
+          <div class="row aboutrow">
+            <div class="col-md-6" style="text-align: left;">
+              <p>Hi! I am Abdul Wahhaab Abrahams, but you can call me Wahhaab. I'm an aspiring fullstack Web Developer that
+                enjoys helping out wherever I can and sharing the knowledge that i've gained as well as take in new
+                information and learn more. I'm a bit of a perfectionist at times and try to recreate things exactly as I
+                envisioned it, but i see that as a strength and not a weakness.<br/>
+                When I am not in front of the computer coding, I spend my time playing basketball🏀 working out at the
+                gym🏋️‍♂️playing games with my friends🎮and spending time with my family👪.</p>
+            </div>
+            <div class="col-md-6 deats">
+              <p class="things">
+                Full name: Abdul Wahhaab Abrahams<br/>
+                Date of birth: 12 May 2002<br/>
+                Location: Cape Town<br/>
+                Cell Phone number: 083 400 4653<br/>
+                Email Address: abdulabrahams2002@gmail.com
+              </p>
+            </div>
           </div>
-        </div>
-      </section>
-      <div class="space" id="about"></div>
-      <section class="aboutSec">
-        <h1 class="AboutH1">About Me</h1>
-        <div class="row aboutrow">
-          <div class="col-md-6" style="text-align: left;">
-            <p>Hi! I am Abdul Wahhaab Abrahams, but you can call me Wahhaab. I'm an aspiring fullstack Web Developer that
-              enjoys helping out wherever I can and sharing the knowledge that i've gained as well as take in new
-              information and learn more. I'm a bit of a perfectionist at times and try to recreate things exactly as I
-              envisioned it, but i see that as a strength and not a weakness.<br/>
-              When I am not in front of the computer coding, I spend my time playing basketball🏀 working out at the
-              gym🏋️‍♂️playing games with my friends🎮and spending time with my family👪.</p>
+        </section>
+        <div class="space" id="resume"></div>
+        <section>
+          <h1 class="ResumeH1">Resume</h1>
+          <div class="resCont" style="display:inline-block;">
+              <div class="justify-content-center mx-2">
+              <div class="row my-5 resRow" v-for="person in resume">
+                <div class="col-md-6" style="text-align: left;">
+                          <h6 class="my-4 text-primary" style=" margin-bottom: 20px;">📅  {{ person.period }}</h6>
+                    <h2>{{ person.name }}</h2>
+                    <div class="desc">{{ person.description}}</div>
+                  </div>
+                  <div class="col-md-6 imgCol">
+                    <img :src="person.image" alt="">
+                  </div>
           </div>
-          <div class="col-md-6 deats">
-            <p class="things">
-              Full name: Abdul Wahhaab Abrahams<br/>
-              Date of birth: 12 May 2002<br/>
-              Location: Cape Town<br/>
-              Cell Phone number: 083 400 4653<br/>
-              Email Address: abdulabrahams2002@gmail.com
-            </p>
           </div>
-        </div>
-      </section>
-      <div class="space" id="resume"></div>
-      <section>
-        <h1 class="ResumeH1">Resume</h1>
-        <div class="resCont" style="display:inline-block;">
-            <div class="justify-content-center mx-2">
-            <div class="row my-5 resRow" v-for="person in resume">
-              <div class="col-md-6" style="text-align: left;">
-                        <h6 class="my-4 text-primary" style=" margin-bottom: 20px;">📅  {{ person.period }}</h6>
-                  <h2>{{ person.name }}</h2>
-                  <div class="desc">{{ person.description}}</div>
-                </div>
-                <div class="col-md-6 imgCol">
-                  <img :src="person.image" alt="">
-                </div>
-        </div>
-        </div>
-        </div>
-        <h1 class="skillsH1">Skills</h1>
-        <div class="container-lg" style="display:inline-block;">
-            <div class="row justify-content-center mx-2">
-            <div class="col-md-3 mx-2 my-2 skillsCol" v-for="pic in skills">
-                      <img :src="pic.image" alt="">
-        </div>
-        </div>
-        </div>
-      </section>
-      <div class="space" id="testimonials"></div>
-      <section>
-        <Testimonials/>
-      </section>
-      <div class="space" id="projects"></div>
-      <section>
-        <Projects/>
-      </section>
-      <div class="space" id="contact"></div>
-      <section>
-        <Contact/>
-      </section>
+          </div>
+          <h1 class="skillsH1">Skills</h1>
+          <div class="container-lg" style="display:inline-block;">
+              <div class="row justify-content-center mx-2">
+              <div class="col-md-3 mx-2 my-2 skillsCol" v-for="pic in skills">
+                        <img :src="pic.image" alt="">
+          </div>
+          </div>
+          </div>
+        </section>
+        <div class="space" id="testimonials"></div>
+        <section>
+          <Testimonials/>
+        </section>
+        <div class="space" id="projects"></div>
+        <section>
+          <Projects/>
+        </section>
+        <div class="space" id="contact"></div>
+        <section>
+          <Contact/>
+        </section>
+      </div>
+      <Footer/>
     </div>
-    <Footer/>
+  </div>
+
+  <div v-else>
+    <Spinner/>
   </div>
 </template>
 
@@ -86,9 +92,10 @@ import Testimonials from '../components/Testimonials.vue'
 import Projects from '../components/Projects.vue'
 import Contact from '../components/Contact.vue'
 import Footer from '../components/Footer.vue'
+import Spinner from '../components/Spinner.vue'
 export default {
   name: 'HomeView',
-  components: { Navbar, Testimonials, Projects, Contact, Footer },
+  components: { Navbar, Testimonials, Projects, Contact, Footer, Spinner },
       data(){
               return{
                   resume:[
