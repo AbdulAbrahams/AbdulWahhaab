1c178c3a1a635dd940295e35502a8ad40cfeec95
AbdulAbrahams <114492995+AbdulAbrahams@users.noreply.github.com>
2025-11-18 14:33:11 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,7 +4,7 @@
       <div class="container">
         <section class="introSec">
           <div class="row introduction">
-            <div class="col-md-6 intro" data-aos="fade-left" data-aos-duration="2000">
+            <div class="col-md-4 intro" data-aos="fade-left" data-aos-duration="2000">
               <h1 class="introH1">Abdul Wahhaab Abrahams</h1>
               <h3 class="my-1">QA Analyst/Automation Engineer</h3>
               <a href="#about" class="learn btn btn-lg btn-light my-4 px-5" style="border-radius: 0px;">Learn About Me</a>
@@ -14,7 +14,7 @@
                 <a href="https://github.com/AbdulAbrahams"><i class="fa fa-github mx-4"></i></a>
               </div> 
             </div>
-               <div class="col-md-6 meImg" data-aos="fade-right" data-aos-duration="2000">
+               <div class="col-md-8 meImg" data-aos="fade-right" data-aos-duration="2000">
                 <img src="https://i.postimg.cc/YqJKrPkn/Me-Image.png" alt="">
               </div>  
           </div>
